3394d29ceb37e79918680be26541a0d9887d8ca7
Manuel Ventura <manuel.ventura@ohtic.com>
2024-05-21 20:53:25 +0200

    "Second Commit"

---
+++ src/main/java/com/inespay/integration/PruebaController.java
@@ -0,0 +1,15 @@
+package com.inespay.integration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class PruebaController {
+
+    @GetMapping
+    public String getHolaMundo(){
+        return "Hola mundo";
+    }
+}
